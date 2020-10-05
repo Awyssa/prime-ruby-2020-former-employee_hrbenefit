19bed3dbc2bcc079bee33b7b8ed91477d3099886
Michael Ernest <m.hendersun@gmail.com>
2020-10-05 21:06:30 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,11 +2,8 @@ require 'prime'
 
 def prime?(n)
 
-  n.map do |i|
-    if i.prime?
-      true
-    else
-      return false
-    end
-  end
+  if n.prime?
+    return true
+  else
+    return false
 end
